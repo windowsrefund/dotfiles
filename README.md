2be07fd522e10f4c8639800ea955bd417c986c5d
windowsrefund <mtf8>
2019-08-23 00:23:12 -0400

    the beginning

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# dotfiles
-Now using dotdrops
+# Dotfiles
+This time, I'll manage with [dotdrop](https://github.com/deadc0de6/dotdrop).
+
+# Install
+``` shell
+./bootstrap.sh
+```
+
---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+error() {
+  echo $@
+  exit 1
+}
+
+[ -f /etc/debian_version ] && OS=debian
+
+case $OS in
+  debian)
+    read -p "Install termite? (y/N): "
+    [[ "$REPLY" == [Yy] ]] && ./termite-install.sh
+    . packages.debian
+    INSTALL="apt install"
+    REMOVE="apt -f remove"
+    ;;
+  *)
+    error Unsupported OS
+    ;;
+esac
+
+sudo $REMOVE $PACKAGES_REMOVE
+sudo $INSTALL $PACKAGES_INSTALL
+
+read -p "Install dotdrop? (y/N): "
+[[ "$REPLY" == [Yy] ]] && pip3 install --user dotdrop
