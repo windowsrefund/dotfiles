45232983e623c5bcdae8f72e5e19e6587f5933d1
Adam Kosmin <akosmin@libretop>
2019-09-27 11:27:35 -0400

    improve kicker

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,25 +5,32 @@ error() {
   exit 1
 }
 
-[ -f /etc/debian_version ] && OS=debian
+go() {
+	read -p "Install termite? (y/N): "
+	[[ "$REPLY" == [Yy] ]] && ./termite-install.sh
+	read -p "Manage packages? (y/N): "
+	[[ "$REPLY" == [Yy] ]] && {
+		sudo $REMOVE $PACKAGES_REMOVE
+		sudo $INSTALL $PACKAGES_INSTALL
+	}
+	read -p "Install dotdrop? (y/N): "
+	[[ "$REPLY" == [Yy] ]] && pip3 install --user dotdrop
+}
+
+[ -f /etc/debian_version ] && {
+	OS=debian
+	INSTALL="apt install"
+	REMOVE="apt -f remove"
+	. packages.debian
+}
 
 case $OS in
   debian)
-    read -p "Install termite? (y/N): "
-    [[ "$REPLY" == [Yy] ]] && ./termite-install.sh
-    . packages.debian
-    INSTALL="apt install"
-    REMOVE="apt -f remove"
+    go
     ;;
   *)
     error Unsupported OS
     ;;
 esac
-
-sudo $REMOVE $PACKAGES_REMOVE
-sudo $INSTALL $PACKAGES_INSTALL
-
-read -p "Install dotdrop? (y/N): "
-[[ "$REPLY" == [Yy] ]] && pip3 install --user dotdrop
 
 mkdir ~/bin 2> /dev/null
