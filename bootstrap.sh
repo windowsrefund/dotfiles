a5d7a28b52ff8a42d8b47b8455dda590684c4ab4
windowsrefund <mtf8>
2019-12-11 23:38:12 -0500

    config neovim

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,6 +32,12 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 /usr/bin/vim.basic +PLuginInstall +qall
 }
 
+read -p "Configure neovim? (y/N): "
+[[ "$REPLY" == [Yy] ]] && {
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
+
 mkdir ~/bin 2> /dev/null
 wget -q -O ~/bin/safe.sh https://raw.githubusercontent.com/windowsrefund/safe/master/safe.sh
 
