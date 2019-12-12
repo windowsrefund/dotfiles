27981fe06cc9c66d2184ee4c8c5552154ff2016c
windowsrefund <mtf8>
2019-12-11 21:56:30 -0500

    various cleanups

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,36 +5,28 @@ error() {
   exit 1
 }
 
-go() {
-	read -p "Install termite? (y/N): "
-	[[ "$REPLY" == [Yy] ]] && ./termite-install.sh
-	read -p "Manage packages? (y/N): "
-	[[ "$REPLY" == [Yy] ]] && {
-		sudo $REMOVE $PACKAGES_REMOVE
-		sudo $INSTALL $PACKAGES_INSTALL
-	}
-  read -p "Install python packages? (y/N): "
-	[[ "$REPLY" == [Yy] ]] && {
-    pip3 install --user \
-    dotdrop \
-    python-language-server[all]
-  }
-}
-
-[ -f /etc/debian_version ] && {
-	OS=debian
-	INSTALL="apt install"
-	REMOVE="apt -f remove"
-	. packages.debian
-}
+[ -f /etc/debian_version ] && ./debian.conf
 
 case $OS in
   debian)
-    go
+    read -p "Install termite? (y/N): "
+    [[ "$REPLY" == [Yy] ]]  && ./termite-install.sh
+
+    read -p "Manage packages? (y/N): "
+    [[ "$REPLY" == [Yy] ]] && {
+        sudo $REMOVE $PACKAGES_REMOVE
+        sudo $INSTALL $PACKAGES_INSTALL
+
+    read -p "Install python packages? (y/N): "
+    [[ "$REPLY" == [Yy] ]] && ./pip-packages.sh
+
     ;;
   *)
     error Unsupported OS
     ;;
 esac
 
+
+
 mkdir ~/bin 2> /dev/null
+wget -q -O ~/bin/safe.sh https://raw.githubusercontent.com/windowsrefund/safe/master/safe.sh
---
+++ pip-packages.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+pip3 install --user \
+  dotdrop \
+  python-language-server[all] \
+  sshoot
