105a0463aa251d1066042f4a050997005f8be8be
windowsrefund <mtf8>
2020-03-26 22:14:46 -0400

    wal bits

---
+++ dotfiles/cache/wal/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P008090B
+                 \e]P1966471
+                 \e]P2637894
+                 \e]P3a17182
+                 \e]P46a789c
+                 \e]P5b28ca1
+                 \e]P6c8afac
+                 \e]P7dfe4e3
+                 \e]P89c9f9e
+                 \e]P9966471
+                 \e]PA637894
+                 \e]PBa17182
+                 \e]PC6a789c
+                 \e]PDb28ca1
+                 \e]PEc8afac
+                 \e]PFdfe4e3
+                 \ec'
